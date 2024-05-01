77cbdd7eed3a371f86adccc59eb38f0039c6492b
omwitsa-etole <omwitsaetole@gmail.com>
2024-05-01 08:43:56 +0300

    "edit add exam"

--- routes/coursedetail.js
+++ routes/coursedetail.js
@@ -9,7 +9,7 @@ router.get('/:id', auth,async (req, res) => {
     const course  = await fetchFunction(req.session.token,resource.SERVER+"/hapi/classes/data/"+req.params.id,"get",null,function(data,e){
         console.log("data=>",data)
         if(data !== undefined){
-            result.courses = [data.course]
+            result.courses = [data.class]
             result.subjects = data.subjects
             result.user = data.user
             return data
@@ -22,11 +22,12 @@ router.get('/:id', auth,async (req, res) => {
  
     console.log("result=>",result)
 	
-	req.session.subjects = result.subjects
+	req.session.subjects = result.subjects ? result.subjects:req.session.subjects
+    req.session.course = result.courses[0] ? result.courses : {hcName:undefined}
 	//let token = req.session.token
 	//req.session = result
 	//req.session.token = token
-    res.render('coursedetail\\coursedetail\\course\\main\\index.handlebars', { pageTitle: 'Select Path',user:req.session.user ,session:req.session});
+    res.render('coursedetail\\coursedetail\\course\\main\\index.handlebars', { pageTitle: req.session.course.hcName,user:req.session.user ,session:req.session,course:req.session.course});
 });
   
 router.get('/', auth,async (req, res) => {
